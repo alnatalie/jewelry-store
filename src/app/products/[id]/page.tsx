3316c601a94c7ef0dfcc9cdb09cb0f71812f8060
Natalie <natasha@email.com>
2025-04-13 14:29:16 +0700

    +page.tsx(prosuct[id]

---
+++ src/app/products/[id]/page.tsx
@@ -0,0 +1,7 @@
+import { Product } from "@/shared/entities/Product"
+
+export default function ProductPage({params} : {params:Promise<{id:string}>}) {
+    return <>
+    <div>Product ID  </div>
+    </>
+}
